460fbbce46c8cd6da5d21fb7c0cabb87cc1cafc3
patrick-replogle <patrickr1138@gmail.com>
2019-12-18 17:17:39 -0800

    added contexts and providers for app.

---
+++ src/providers/UserProvider.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { userContext } from "../contexts/userContext";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useLocalStorage("");
+  return (
+    <userContext.Provider value={(user, setUser)}>
+      {children}
+    </userContext.Provider>
+  );
+};
+
+export default UserProvider;
